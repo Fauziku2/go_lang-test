bd00b6ec9363494ec99885ffd75babe5a4accf62
Fauzi Mohd Ghouse <m.fauzi_87@hotmail.com>
2019-07-26 12:11:15 +0800

    create acdc folder

--- main.go
+++ main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Fauziku2/go_lang-test/dog"
 )
 
-type canine struct {
-	name string
-	age  int
-}
-
 func main() {
-	fido := canine{
-		name: "Fido",
-		age:  dog.Years(10),
-	}
-
-	fmt.Println(fido)
+	fmt.Println(acdc.Sum(2, 3))
+	fmt.Println(acdc.Sum(2, 3, 4, 5, 6, 7, 8, 9))
 }
